087f0a59a29c7c038277d71b75be0371c320a4bb
Cody Mac <nicoaudy1994@gmail.com>
2019-06-24 21:08:00 +0700

    add readme

---
+++ readme.md
@@ -0,0 +1,17 @@
+## Story
+---
+
+Instead of repeating my self setup react native project from ground up, at the end im thinking to make my life easier then making this some boilerplate for personal used 🚀:metal: 
+
+### Installed Package
+
+- react navigation
+- mobx
+- mobx react
+- @babel decorator
+  
+
+### Screenshot
+---
+
+![Screenshot](https://raw.githubusercontent.com/nicoaudy/rn-mobx-boilerplate/master/src/assets/screenshot/splash.png)
--- src/screens/splash/index.js
+++ src/screens/splash/index.js
@@ -1,18 +1,92 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { View, StyleSheet, Animated, Dimensions } from "react-native";
 
-export default class Splash extends Component {
+const { width } = Dimensions.get("window");
+
+export default class SplashScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.animatedValue = new Animated.Value(0.5);
+    this.animatedValue2 = new Animated.Value(0);
+  }
+
   componentDidMount() {
+    Animated.spring(this.animatedValue, {
+      toValue: 1,
+      friction: 4,
+      delay: 1500
+    }).start();
+
+    Animated.timing(this.animatedValue2, {
+      toValue: 1,
+      delay: 200,
+      duration: 1000
+    }).start();
+
     setTimeout(() => {
       this.props.navigation.navigate("LOGIN");
-    }, 2000);
+    }, 5000);
   }
 
   render() {
+    const truckStyle = {
+      transform: [{ scale: this.animatedValue }]
+    };
+
+    const scaleText = {
+      transform: [{ scale: this.animatedValue2 }]
+    };
+
     return (
-      <View>
-        <Text> SplashScreen </Text>
+      <View style={styles.container}>
+        <Animated.View style={[styles.ring, truckStyle]}>
+          <Animated.Image
+            source={require("assets/logo.png")}
+            style={[
+              {
+                resizeMode: "contain",
+                width: 200,
+                height: 200
+              }
+            ]}
+          />
+        </Animated.View>
+
+        <Animated.View
+          style={[
+            {
+              position: "absolute",
+              bottom: 20,
+              width: width / 2,
+              height: 4,
+              backgroundColor: "#fff",
+              borderRadius: 2
+            },
+            scaleText
+          ]}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#0277BD"
+  },
+  ring: {
+    backgroundColor: "#40C4FF",
+    borderRadius: 150,
+    borderWidth: 2,
+    borderColor: "#FFF",
+    padding: 7
+  },
+  starStyle: {
+    width: 100,
+    height: 20,
+    marginBottom: 20
+  }
+});
