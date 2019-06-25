1d224fa10ff176a10f6f47ba4210b6df204c5e38
codyway <nico.audi@intidata.net>
2019-06-25 14:39:43 +0700

    change color if error on input

--- src/components/StyledInput/input.js
+++ src/components/StyledInput/input.js
@@ -2,24 +2,32 @@ import React from "react";
 
 import { View, Text, TextInput } from "react-native";
 
-const StyledInput = ({ label, formikProps, formikKey, ...rest }) => (
-  <View style={{ marginHorizontal: 20, marginVertical: 5 }}>
-    <Text style={{ marginBottom: 3 }}>{label}</Text>
-    <TextInput
-      style={{
-        borderWidth: 1,
-        borderColor: "black",
-        padding: 10,
-        marginBottom: 3
-      }}
-      onChangeText={formikProps.handleChange(formikKey)}
-      onBlur={formikProps.handleBlur(formikKey)}
-      {...rest}
-    />
-    <Text style={{ color: "red" }}>
-      {formikProps.touched[formikKey] && formikProps.errors[formikKey]}
-    </Text>
-  </View>
-);
+const StyledInput = ({ label, formikProps, formikKey, ...rest }) => {
+  const inputStyle = {
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 10,
+    marginBottom: 3
+  };
+
+  if (formikProps.touched[formikKey] && formikProps.errors[formikKey]) {
+    inputStyle.borderColor = "red";
+  }
+
+  return (
+    <View style={{ marginHorizontal: 20, marginVertical: 5 }}>
+      <Text style={{ marginBottom: 3 }}>{label}</Text>
+      <TextInput
+        style={inputStyle}
+        onChangeText={formikProps.handleChange(formikKey)}
+        onBlur={formikProps.handleBlur(formikKey)}
+        {...rest}
+      />
+      <Text style={{ color: "red" }}>
+        {formikProps.touched[formikKey] && formikProps.errors[formikKey]}
+      </Text>
+    </View>
+  );
+};
 
 export default StyledInput;
